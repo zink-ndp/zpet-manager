ea510b25f460b5dafdfe2d9786bf748afb5f2e47
zink-ndp <phucvinhlp02@gmail.com>
2024-02-25 10:22:49 +0700

    beta make invoice

--- src/app/manager/contents/makeInvoice/MakeInvoice.tsx
+++ src/app/manager/contents/makeInvoice/MakeInvoice.tsx
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from "react";
-import Autocomplete from "@mui/joy/Autocomplete";
+import Autocomplete, { createFilterOptions } from "@mui/joy/Autocomplete";
 import Checkbox from "@mui/joy/Checkbox";
 import { getToday } from "@/app/functions";
-import Chip from "@mui/joy/Chip";
-import { Close } from "@mui/icons-material";
+import { getSessionData } from "@/app/session/getSession";
 import { List, ListDivider, ListItem, Radio, RadioGroup } from "@mui/joy";
+import axios from "axios";
+import { formatMoney } from "@/app/functions";
 
 export default function MakeInvoice() {
+  const testOption = ["opt1", "opt2", "opt3"];
   const today = getToday();
 
-  const staffName = "Nguyen Van V";
-  const staffId = "92837";
+  const [session, setSession] = useState<Array<any> | null>(null);
+  const [cusList, setCusList] = useState<Array<any> | null>(null);
+  const [cusInfo, setCusInfo] = useState<Array<any> | null>(null);
+  const [petList, setPetList] = useState<Array<any> | null>(null);
+  const [srvList, setSrvList] = useState<Array<any> | null>(null);
 
-  const testOption = ["opt1", "opt2", "opt3"];
+  function resetAll(){
+    setPhone('')
+    setCusName('')
+    setPet('')
+    setService([])
+    setShipPet(false)
+    setAddress('')
+    setPrice(0)
+    setTotal(0)
+    setBtnDisabled(true)
+  }
 
   const [phone, setPhone] = useState<String | null>("");
   const [cusName, setCusName] = useState<String | null>("");
@@ -21,19 +36,137 @@ export default function MakeInvoice() {
   const [shipPet, setShipPet] = useState(false);
   const [address, setAddress] = useState<String | null>("");
   const [price, setPrice] = useState(0);
-  const [payment, setPayment] = useState("");
   const [total, setTotal] = useState(0);
+  const [btnDisabled, setBtnDisabled] = useState(true);
 
-  const [btnEnable, setBtnEnable] = useState(false);
+  const fetchCustomers = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3100/api/v1/customers"
+      );
+      const data = await response.data.data;
+      setCusList(data);
+    } catch (error) {}
+  };
+
+  const fetchCustomerByPhone = async (p: String) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3100/api/v1/customers/info/${p}`
+      );
+      const data = await response.data.data;
+      setCusInfo(data);
+    } catch (error) {}
+  };
+
+  const fetchPets = async (id: String) => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3100/api/v1/customers/" + id + "/pets"
+      );
+      const data: any = await response.data.data;
+      setPetList(data);
+    } catch (err: any) {}
+  };
+
+  const fetchServices = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3100/api/v1/services/"
+      );
+      const data: any = await response.data.data;
+      setSrvList(data);
+    } catch (err: any) {}
+  };
 
   useEffect(() => {
-    function checkFormOk() {
-      if (!phone || !cusName || !pet || !service || !payment) return false;
-      return true;
+    getSessionData().then((value) => {
+      setSession(value);
+    });
+    fetchCustomers();
+    fetchServices();
+  }, []);
+
+  const staffName = session ? session[0].STF_NAME : "";
+  const staffId = session ? session[0].STF_ID : "";
+
+  const nameList: any = [];
+  const phoneList: any = [];
+  cusList?.forEach((cus) => {
+    nameList.push(cus.CTM_NAME + " - Mã:" + cus.CTM_ID);
+    phoneList.push(cus.CTM_PHONE);
+  });
+
+  useEffect(() => {
+    if (phone?.length == 10) fetchCustomerByPhone(phone);
+  }, [phone]);
+
+  useEffect(() => {
+    if (cusInfo) {
+      setCusName(cusInfo[0].CTM_NAME + " - Mã:" + cusInfo[0].CTM_ID);
+      fetchPets(cusInfo[0].CTM_ID);
     }
-    const isOk = checkFormOk();
-    isOk ? setBtnEnable(true) : setBtnEnable(false);
-  }, [phone, cusName, pet, service, payment]);
+  }, [cusInfo]);
+
+  useEffect(() => {
+    setPrice(0);
+    service?.forEach((srv: String) => {
+      srvList?.forEach((s) => {
+        const s_name = s.SRV_NAME;
+        if (s_name == srv.split("-")[0]) {
+          setPrice(price + s.SRV_PRICE);
+        }
+      });
+    });
+  }, [service]);
+
+  useEffect(()=>{
+    setTotal(price)
+  },[price])
+
+  useEffect(() => {
+    if (!cusName || !pet || !service) setBtnDisabled(true);
+    else setBtnDisabled(false);
+  }, [cusName, pet, service]);
+
+  const petNameList: any = [];
+  petList?.forEach((pet) => {
+    petNameList.push(pet.P_NAME + "-Mã:" + pet.P_ID);
+  });
+
+  const srvNameList: any = [];
+  srvList?.forEach((srv) => {
+    srvNameList.push(srv.SRV_NAME + "-Mã:" + srv.SRV_ID);
+  });
+
+  function handleMakeInvoice() {
+    if (session && cusName && pet && phone && service) {
+      const today = new Date();
+      const time =
+        today.getHours() +
+        ":" +
+        today.getMinutes() +
+        ":" +
+        today.getSeconds() +
+        " - " +
+        today.getDate() +
+        "/" +
+        (today.getMonth() + 1) +
+        "/" +
+        today.getFullYear();
+      const stfId = session[0]!.STF_ID;
+      const cusId = cusName?.split(":")[1];
+      const petId = pet.split(":")[1];
+      const srvId: Array<string> = [];
+      service.forEach((srv: string) => {
+        const id = srv.split(":")[1].toString();
+        srvId.push(id);
+      });
+      const tt = total
+      console.log(time, stfId, cusId, petId, srvId, tt);
+      resetAll()
+    } else console.log("Chua du thong tin");
+  }
 
   return (
     <>
@@ -45,36 +178,37 @@ export default function MakeInvoice() {
         <p className="text-lg mt-4">
           Nhân viên:{" "}
           <p className="font-bold">
-            {staffName} - {staffId}
+            {staffName} - Mã NV: {staffId}
           </p>
         </p>
         <p className="text-lg mt-4">Số điện thoại</p>
         <Autocomplete
           placeholder="Số điện thoại khách hàng"
-          options={testOption}
+          options={phoneList}
           value={phone}
           className="w-full h-[50px]"
-          onChange={(event, nValue) => {
+          onInputChange={(e, nValue) => {
             setPhone(nValue);
           }}
         />
         <p className="text-lg mt-4">Tên khách hàng</p>
         <Autocomplete
           placeholder="Tên khách hàng"
-          options={testOption}
+          options={nameList}
           className="w-full h-[50px]"
           value={cusName}
-          onChange={(event, nValue) => {
+          onInputChange={(e, nValue) => {
             setCusName(nValue);
           }}
         />
         <p className="text-lg mt-4">Thú cưng</p>
         <Autocomplete
+          disabled={petList ? false : true}
           placeholder="Thú cưng của Khách hàng"
-          options={testOption}
+          options={petNameList}
           className="w-full h-[50px]"
           value={pet}
-          onChange={(event, nValue) => {
+          onInputChange={(e, nValue) => {
             setPet(nValue);
           }}
         />
@@ -84,17 +218,17 @@ export default function MakeInvoice() {
           placeholder="Dịch vụ"
           limitTags={4}
           size="lg"
-          options={testOption}
-          getOptionLabel={(option) => option}
+          options={srvNameList}
+          getOptionLabel={(option: string) => option}
           onChange={(event, nValue) => {
             setService(nValue);
-            setPrice(price + 1);
           }}
           className="w-full h-[50px]"
         />
 
         <p className="text-lg mt-4">
-          Đơn giá dịch vụ tạm tính: <p className="font-bold">{price}đ</p>
+          Đơn giá dịch vụ tạm tính:{" "}
+          <p className="font-bold">{formatMoney(price)}</p>
         </p>
         <Checkbox
           className="text-xl mt-4 font-semi-bold"
@@ -117,7 +251,7 @@ export default function MakeInvoice() {
           </div>
         )}
 
-        {(() => {
+        {/* {(() => {
           if (price)
             return (
               <>
@@ -154,23 +288,18 @@ export default function MakeInvoice() {
                 </RadioGroup>
               </>
             );
-        })()}
+        })()} */}
 
-        {(() => {
-          if (payment == "QR Code") return <p className="text-black">QRCODE</p>;
-        })()}
-
-        <ListDivider sx={{marginTop: "15px"}} />
+        <ListDivider sx={{ marginTop: "15px" }} />
         <p className="text-xl mt-4">
-          Tổng tiền phải thanh toán: <p className="font-bold">{total}</p>
+          Tổng tiền phải thanh toán: <p className="font-bold">{formatMoney(total)}</p>
         </p>
 
         <button
-          className={`justify-center mt-5 ${
-            btnEnable ? "primary-btn" : "primary-disabled-btn"
-          } `}
+          disabled={btnDisabled}
+          className="mt-4 primary-btn disabled:primary-disabled-btn"
           onClick={(e) => {
-            console.log(cusName, service, phone, payment);
+            handleMakeInvoice();
           }}
         >
           Lập hoá đơn
--- src/app/manager/page.tsx
+++ src/app/manager/page.tsx
@@ -28,7 +28,6 @@ export default function Page() {
   useEffect(()=>{
     getSessionData().then( (value) => {
       setSession(value)
-      console.log(value)
     })
   },[])
 
