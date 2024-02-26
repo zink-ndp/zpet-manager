c7dcc930441e2899097cf91e0ecbfc3ed2644bff
zink-ndp <phucvinhlp02@gmail.com>
2024-02-26 14:47:05 +0700

    make new appointment

--- src/app/manager/contents/dashboard/appointments/NewAppoinment.tsx
+++ src/app/manager/contents/dashboard/appointments/NewAppoinment.tsx
@@ -5,15 +5,65 @@ import { DateTimePicker, MobileDateTimePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
+import axios from "axios";
 
 export default function NewAppoinment() {
   const testOption = ["opt1", "opt2", "opt3"];
 
-  const [dateTime, setDateTime] = useState('');
+  const [dateTime, setDateTime] = useState<any>();
   const [phone, setPhone] = useState<String | null>("");
   const [cusName, setCusName] = useState<String | null>("");
   const [service, setService] = useState<String[] | any>();
-  const [btnEnable, setBtnEnable] =  useState(false)
+  const [note, setNote] = useState<String | null>("");
+  const [btnEnable, setBtnEnable] = useState(false);
+
+  const [cusList, setCusList] = useState<Array<any>>();
+  const [srvList, setSrvList] = useState<Array<any>>();
+
+  const fetchCustomer = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3100/api/v1/customers"
+      );
+      const data = await response.data.data;
+      setCusList(data);
+    } catch (error) {}
+  };
+
+  const fetchCustomerByPhone = async (p: String) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3100/api/v1/customers/info/${p}`
+      );
+      const data = await response.data.data;
+      setCusName(data[0].CTM_NAME + " - Mã:" + data[0].CTM_ID);
+    } catch (error) {}
+  };
+
+  const fetchServices = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3100/api/v1/services/"
+      );
+      const data: any = await response.data.data;
+      setSrvList(data);
+    } catch (err: any) {}
+  };
+
+  useEffect(() => {
+    fetchCustomer();
+    fetchServices();
+  }, []);
+
+  const cusPhoneList: any = [];
+  cusList?.forEach((cus) => {
+    cusPhoneList.push(cus.CTM_PHONE);
+  });
+
+  const srvNameList: any = [];
+  srvList?.forEach((srv) => {
+    srvNameList.push(srv.SRV_NAME + " - Mã:" + srv.SRV_ID);
+  });
 
   useEffect(() => {
     function checkFormOk() {
@@ -24,6 +74,17 @@ export default function NewAppoinment() {
     isOk ? setBtnEnable(true) : setBtnEnable(false);
   }, [phone, cusName, service, dateTime]);
 
+  function handleAddAppointment() {
+    const date = dateTime.$D + "/" + (dateTime.$M + 1) + "/" + dateTime.$y;
+    const time = dateTime.$H + ":" + dateTime.$m;
+    const cusId = cusName?.split(":")[1]
+    const srvs: string[] = []
+    service.forEach((srv: string) => {
+      srvs.push(srv.split(":")[1])
+    });
+    console.log(date, time, cusId, srvs, note);
+  }
+
   return (
     <div className="flex flex-col p-4">
       <p className="text-xl font-bold mt-4 self-center">Thêm lịch hẹn</p>
@@ -31,24 +92,27 @@ export default function NewAppoinment() {
         <MobileDateTimePicker
           className="w-full h-[45px] mt-7"
           label="Ngày, giờ hẹn"
-          onChange={(v:any) => setDateTime(v)}
+          disablePast
+          onChange={(v: any) => setDateTime(v)}
         />
       </LocalizationProvider>
       <p className="text-lg mt-4">Số điện thoại</p>
       <Autocomplete
         placeholder="Số điện thoại khách hàng"
-        options={testOption}
+        options={cusPhoneList}
         value={phone}
         className="w-full h-[50px]"
         freeSolo
-        onChange={(event, nValue) => {
+        onInputChange={(event, nValue) => {
           setPhone(nValue);
+          fetchCustomerByPhone(nValue);
         }}
       />
       <p className="text-lg mt-4">Tên khách hàng</p>
       <Autocomplete
         placeholder="Tên khách hàng"
         options={testOption}
+        disabled
         value={cusName}
         className="w-full h-[50px]"
         freeSolo
@@ -62,26 +126,32 @@ export default function NewAppoinment() {
         placeholder="Dịch vụ"
         limitTags={4}
         size="lg"
-        options={testOption}
+        options={srvNameList}
         freeSolo={true}
-        getOptionLabel={(option) => option}
+        getOptionLabel={(option: string) => option}
         onChange={(event, nValue) => {
           setService(nValue);
         }}
         className="w-full h-[50px]"
       />
       <p className="text-lg mt-4">Ghi chú:</p>
-      <Textarea placeholder="Ghi chú cho cửa hàng…" minRows={3} />
+      <Input
+        className="w-full h-[50px]"
+        placeholder="Ghi chú cho cửa hàng…"
+        onChange={(e: any)=>{
+          setNote(e.target.value)
+        }}
+      />
       <button
-          className={`justify-center mt-5 ${
-            btnEnable ? "primary-btn" : "primary-disabled-btn"
-          } `}
-          onClick={(e) => {
-            console.log(dateTime, cusName, service, phone);
-          }}
-        >
-          Lập hoá đơn
-        </button>
+        className={`justify-center mt-5 ${
+          btnEnable ? "primary-btn" : "primary-disabled-btn"
+        } `}
+        onClick={(e) => {
+          handleAddAppointment();
+        }}
+      >
+        Lập hoá đơn
+      </button>
     </div>
   );
 }
